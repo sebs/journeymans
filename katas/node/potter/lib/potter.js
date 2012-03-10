52391098b0f2f4fecc1e037d6eced83b1ff6c994
Sebastian Schürmann <sebs@2xs.org>
2012-03-10 14:30:15 +0100

    - price is calculated

--- katas/node/potter/lib/potter.js
+++ katas/node/potter/lib/potter.js
@@ -1,3 +1,7 @@
 exports.getPrice = function() {
-
+  var price = 0;
+  for (var i in arguments) {
+	  price += 8;
+	}
+	return price;
 }
--- katas/node/potter/tests/test01.js
+++ katas/node/potter/tests/test01.js
@@ -1,7 +1,10 @@
 var assert = require('assert');
 var cart = require('../lib/potter');
 describe('Basiscs', function () {
-  it('should return something', function() { 
+  it('should should be function', function() { 
 	  assert.ok(typeof cart.getPrice == 'function'); 
+	});
+	it('should calculate a price for one numeric argument', function() {
+    assert.equal(cart.getPrice(1), 8);	
 	});
 });
