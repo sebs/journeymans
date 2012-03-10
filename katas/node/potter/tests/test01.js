3661d75102f9d02f18442c93fb9be56481ae7762
Sebastian Schürmann <sebs@2xs.org>
2012-03-10 14:43:10 +0100

    - 2 books 16

--- katas/node/potter/tests/test01.js
+++ katas/node/potter/tests/test01.js
@@ -4,7 +4,10 @@ describe('Basiscs', function () {
   it('should should be function', function() { 
 	  assert.ok(typeof cart.getPrice == 'function'); 
 	});
-	it('should calculate a price for one numeric argument', function() {
+	it('should calculate a price for one argument', function() {
     assert.equal(cart.getPrice(1), 8);	
+	});
+  it ('should calc 16 for 2 args', function() {
+	  assert.equal(cart.getPrice(1,1), 16);
 	});
 });
