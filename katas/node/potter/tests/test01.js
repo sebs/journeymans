fe69d128921c1ae94902ccb194896c4da6034ac3
Sebastian Schürmann <sebs@2xs.org>
2012-03-10 14:11:02 +0100

    - Step 1

--- katas/node/potter/tests/test01.js
+++ katas/node/potter/tests/test01.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 var cart = require('../lib/potter');
 describe('Basiscs', function () {
   it('should return something', function() { 
-	
+	  assert.ok(typeof cart.getPrice == 'function'); 
 	});
 });
